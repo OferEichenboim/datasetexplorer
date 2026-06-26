b682e188accccb61dd5176b67577afba2e41429e
ofere <ofer.eichenboim@mobileye.com>
2026-06-27 00:52:26 +0300

    backend-deploy: fix based on what works with render

--- backend/run_prod.sh
+++ backend/run_prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PORT=${PORT:-8000}
 
-# Call uvicorn using the absolute system interpreter path Render just called out
+# Execute using the explicit virtual environment interpreter Render uses
 /opt/render/project/src/.venv/bin/python -m uvicorn main:app --host 0.0.0.0 --port "$PORT" --workers 4
